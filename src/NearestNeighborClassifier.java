ec2138bae9647cb63053a1fe04cb2fe696fa3fe8
Will <wgilles1@emich.edu>
2016-02-28 22:27:55 -0500

    More incremental progress in prepping it for submission

--- src/NearestNeighborClassifier.java
+++ src/NearestNeighborClassifier.java
@@ -55,9 +55,42 @@ public class NearestNeighborClassifier {
 	 */
 	public static void main(String[] args) {
 		/*
-		 * **Part 1
+		 * #2 nearest Neighbor on scores
 		 */
 		NearestNeighborClassifier nnc = new NearestNeighborClassifier();
+		
+		
+		/*
+		 * #3 bank loan risk classification
+		 */
+		String bankTranindDataFileName = "train4";
+		String bankTestDataFileName = "test4";
+		String bankClassifiedRecordsOutputFileName = "output4";
+		nnc = new NearestNeighborClassifier();
+		try {
+			nnc.loadTrainingData(bankTranindDataFileName); // bank training data
+		} catch (Exception e) {	e.printStackTrace();	}
+		
+		/* A: classification of test records */
+		ArrayList<Record> BankTestRecords = null;
+		try {
+			BankTestRecords = nnc.getTestRecordsFromFile(bankTestDataFileName);
+			ArrayList<String> listOfLabels = nnc.classify(BankTestRecords);
+			nnc.writeClassifiedLabelsToFile(bankClassifiedRecordsOutputFileName, listOfLabels);
+		} catch (Exception e) {	e.printStackTrace();	}
+		
+		/* B: training error */
+		double trainingError = nnc.calculateTrainingError();
+		System.out.println("training error: " + trainingError);
+		/* C: one out validation error */
+		double trainErrorOneOut = nnc.calculateTrainingErrorWithLeaveOneOut();
+		System.out.println("One out validation error: " + trainErrorOneOut);
+			
+		/*
+		 * **Part 4: classification of digits
+		 */
+		
+		nnc = new NearestNeighborClassifier(); // start over with new nearest neighbor classifier
 		
 		String[] theFileNames = new String[]{
 				"0_1", "0_2", "0_3", "0_4", "0_5",
@@ -98,44 +131,9 @@ public class NearestNeighborClassifier {
 			fileNumber++;
 		}
 		
-		//System.out.println(nnc);
 		nnc.classify(testRecords);
-		/*
-		try {
-			nnc.loadTrainingData("train4_bank");
-		} catch (Exception e) {
-			System.out.println("error");
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-		System.out.println(nnc.toString());
-		//double trainingError = nnc.calculateTrainingError();
-		//System.out.println("training error: " + trainingError);
-		double trainErrorOneOut = nnc.calculateTrainingErrorWithLeaveOneOut();
-		System.out.println("train error one out: " + trainErrorOneOut);
-		//nnc.writeClassifiedLabelsToFile("output.txt", nnc.classify(nnc.records));
-		 */
-		 
-		/*
-		ArrayList<Record> testRecords = null;
-		try {
-			testRecords = nnc.getTestRecordsFromFile("test3");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		ArrayList<String> listOfLabels = nnc.classify(testRecords);
-		for(int i = 0; i < testRecords.size();i++){
-			testRecords.get(i).label = listOfLabels.get(i);
-			
-		}
-		System.out.println("Test Records classified");
-		for(Record record: testRecords){
-			System.out.println(record);
-		}
-		*/
 	}
+	
 	private final double CATEGORICAL_MATCHING_WEIGHT = 0.4;
 	private final double BINARY_MATCHING_WEIGHT = 0.4;
 
